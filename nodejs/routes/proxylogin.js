93c830e3b30467923dfe424fe335549421807974
Stephen Zhang <stephen.zsy@gmail.com>
2013-08-20 09:21:27 -0700

    proxy for intercepting set-cookies

--- nodejs/routes/proxylogin.js
+++ nodejs/routes/proxylogin.js
@@ -6,13 +6,58 @@
 
 var https = require('https');
 var http = require('http');
-var needle = require('needle');
+var url = require('url')
 
-exports.wsj = function (req, r) {
-    needle.get("http://www.wsj.com", {follow: 10}, function (error, response, body) {
-        r.send(body);
-        r.end();
-        console.log(response.headers);
-    });
-    return;
+exports.proxy = function (req, res) {
+    //console.log(JSON.stringify(req.headers));
+    var parsedUrl = url.parse(req.url);
+
+    var options = {
+        // host to forward to
+        hostname: parsedUrl['hostname'],
+        // port to forward to
+        port: parsedUrl['port'],
+        // path to forward to
+        path: req.url,
+        // request method
+        method: req.method,
+        // headers to send
+        headers: req.headers
+    };
+    delete req.headers['host'];
+    //console.log('requested: ' + options['hostname'] + options['path']);
+    var creq = http.request(options,function (cres) {
+        //console.log('status: ' + cres.statusCode);
+        //console.log('headers: ' + JSON.stringify(cres.headers));
+        var setCookieHeader = cres.headers['set-cookie'];
+        if (setCookieHeader) {
+            console.log(setCookieHeader);
+        }
+        res.writeHead(cres.statusCode, cres.headers);
+
+        // wait for data
+        cres.on('data', function (chunk) {
+            //res.write(chunk, cres.headers['content-encoding']);
+            res.write(chunk);
+        });
+
+        cres.on('end', function () {
+            // console.log('ended');
+            res.end();
+        })
+
+        cres.on('close', function () {
+            // closed, let's end client request as well
+            // res.send(cres.statusCode);
+            //   console.log("closed");
+            res.end();
+        });
+
+    }).on('error', function (e) {
+            // we got an error, return 500 error to client and log error
+            console.log(e.message);
+            //res.send(500);
+            res.end();
+        });
+    creq.end();
 };
